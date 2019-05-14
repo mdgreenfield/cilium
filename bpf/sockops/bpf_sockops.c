mdgreenfield/cilium

commit 01fd58a5d42034382c56eafcd4864d24efdb2af4

bpf: Add support for svc v2 to datapath
This commit introduces svc v2 support to the BPF datapath (to both -
lb and lxc).

First, this makes the svc lookup to rely only on services of
the v2 type. This is safe, as before reloading the datapath,
all v2 services will be created from legacy ones by the daemon.

Second, as we need to support upgrading/downgrading of cilium-agent
without disrupting open connections, both legacy and v2 service
endpoints are stored in CT entries.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=6123c1327efdf336ec9b9c15551b71ae443d5091
+GO_BINDATA_SHA1SUM=f9678534dd09b4a9a5f01b87c13abfdc4f6514fa
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/bpf_lb.c
@@ -56,8 +56,9 @@
 static inline int handle_ipv6(struct __sk_buff *skb)
 {
 	void *data, *data_end;
-	struct lb6_key key = {};
-	struct lb6_service *svc;
+	struct lb6_key_v2 key = {};
+	struct lb6_service_v2 *svc;
+	struct lb6_backend *backend;
 	struct ipv6hdr *ip6;
 	struct csum_offset csum_off = {};
 	int l3_off, l4_off, ret, hdrlen;
@@ -91,21 +92,24 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	}
 #endif
 
-	svc = lb6_lookup_service(skb, &key);
+	svc = lb6_lookup_service_v2(skb, &key);
 	if (svc == NULL) {
 		/* Pass packets to the stack which should not be loadbalanced */
 		return TC_ACT_OK;
 	}
 
-	slave = lb6_select_slave(skb, &key, svc->count, svc->weight);
-	if (!(svc = lb6_lookup_slave(skb, &key, slave)))
+	slave = lb6_select_slave(skb, svc->count, svc->weight);
+	if (!(svc = lb6_lookup_slave_v2(skb, &key, slave)))
+		return DROP_NO_SERVICE;
+	if (!(backend = lb6_lookup_backend(skb, svc->backend_id)))
 		return DROP_NO_SERVICE;
 
-	ipv6_addr_copy(&new_dst, &svc->target);
+	ipv6_addr_copy(&new_dst, &backend->address);
 	if (svc->rev_nat_index)
 		new_dst.p4 |= svc->rev_nat_index;
 
-	ret = lb6_xlate(skb, &new_dst, nexthdr, l3_off, l4_off, &csum_off, &key, svc);
+	ret = lb6_xlate_v2(skb, &new_dst, nexthdr, l3_off, l4_off, &csum_off, &key,
+						svc, backend);
 	if (IS_ERR(ret))
 		return ret;
 
@@ -118,8 +122,9 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 {
 	void *data;
 	void *data_end;
-	struct lb4_key key = {};
-	struct lb4_service *svc;
+	struct lb4_key_v2 key = {};
+	struct lb4_service_v2 *svc;
+	struct lb4_backend *backend;
 	struct iphdr *ip;
 	struct csum_offset csum_off = {};
 	int l3_off, l4_off, ret;
@@ -149,18 +154,21 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	}
 #endif
 
-	svc = lb4_lookup_service(skb, &key);
+	svc = lb4_lookup_service_v2(skb, &key);
 	if (svc == NULL) {
 		/* Pass packets to the stack which should not be loadbalanced */
 		return TC_ACT_OK;
 	}
 
-	slave = lb4_select_slave(skb, &key, svc->count, svc->weight);
-	if (!(svc = lb4_lookup_slave(skb, &key, slave)))
+	slave = lb4_select_slave(skb, svc->count, svc->weight);
+	if (!(svc = lb4_lookup_slave_v2(skb, &key, slave)))
+		return DROP_NO_SERVICE;
+	if (!(backend = lb4_lookup_backend(skb, svc->backend_id)))
 		return DROP_NO_SERVICE;
 
-	new_dst = svc->target;
-	ret = lb4_xlate(skb, &new_dst, NULL, NULL, nexthdr, l3_off, l4_off, &csum_off, &key, svc);
+	new_dst = backend->address;
+	ret = lb4_xlate_v2(skb, &new_dst, NULL, NULL, nexthdr, l3_off, l4_off,
+						&csum_off, &key, svc, backend);
 	if (IS_ERR(ret))
 		return ret;
 

bpf/bpf_lxc.c
@@ -67,8 +67,8 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	int ret, verdict, l4_off, forwarding_reason, hdrlen;
 	struct csum_offset csum_off = {};
 	struct endpoint_info *ep;
-	struct lb6_service *svc;
-	struct lb6_key key = {};
+	struct lb6_service_v2 *svc;
+	struct lb6_key_v2 key = {};
 	struct ct_state ct_state_new = {};
 	struct ct_state ct_state = {};
 	void *data, *data_end;
@@ -89,7 +89,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 
 	l4_off = l3_off + hdrlen;
 
-	ret = lb6_extract_key(skb, tuple, l4_off, &key, &csum_off, CT_EGRESS);
+	ret = lb6_extract_key_v2(skb, tuple, l4_off, &key, &csum_off, CT_EGRESS);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
 			goto skip_service_lookup;
@@ -106,7 +106,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	 * entry for destination endpoints where we can't encode the state in the
 	 * address.
 	 */
-	if ((svc = lb6_lookup_service(skb, &key)) != NULL) {
+	if ((svc = lb6_lookup_service_v2(skb, &key)) != NULL) {
 		ret = lb6_local(get_ct_map6(tuple), skb, l3_off, l4_off,
 				&csum_off, &key, tuple, svc, &ct_state_new);
 		if (IS_ERR(ret))
@@ -404,8 +404,8 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	int ret, verdict, l3_off = ETH_HLEN, l4_off, forwarding_reason;
 	struct csum_offset csum_off = {};
 	struct endpoint_info *ep;
-	struct lb4_service *svc;
-	struct lb4_key key = {};
+	struct lb4_service_v2 *svc;
+	struct lb4_key_v2 key = {};
 	struct ct_state ct_state_new = {};
 	struct ct_state ct_state = {};
 	__be32 orig_dip;
@@ -426,7 +426,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 
 	l4_off = l3_off + ipv4_hdrlen(ip4);
 
-	ret = lb4_extract_key(skb, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
+	ret = lb4_extract_key_v2(skb, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
 			goto skip_service_lookup;
@@ -435,7 +435,9 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	}
 
 	ct_state_new.orig_dport = key.dport;
-	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
+	/* It's safe to lookup the v2 svc instead of the legacy, as the v2 is
+	 * created before the endpoint datapath code has been reloaded. */
+	if ((svc = lb4_lookup_service_v2(skb, &key)) != NULL) {
 		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
 				&key, &tuple, svc, &ct_state_new, ip4->saddr);
 		if (IS_ERR(ret))

bpf/lib/common.h
@@ -565,7 +565,8 @@ struct ct_state {
 	__be32 addr;
 	__be32 svc_addr;
 	__u32 src_sec_id;
-	__u16 slave;
+	__u16 slave;		/* Slave slot number in a legacy service */
+	__u16 backend_id;	/* Backend ID in lb4_backends */
 };
 
 /* Lifetime of a proxy redirection entry. All proxies should be using TCP

bpf/lib/conntrack.h
@@ -225,6 +225,16 @@ static inline int __inline__ __ct_lookup(void *map, struct __sk_buff *skb,
 			ct_state->rev_nat_index = entry->rev_nat_index;
 			ct_state->loopback = entry->lb_loopback;
 			ct_state->slave = entry->slave;
+			/* As we currently support both types of services (in the code
+			 * referred as "legacy" and "v2"), we store references to both
+			 * types of service endpoints. For the legacy, a slave slot
+			 * number is stored within the "ct_entry.slave" field, while
+			 * for the v2, a backend id is stored in the "ct_entry.rx_bytes"
+			 * field which is not used by a entry with dir=CT_SERVICE.
+			 */
+			if (dir == CT_SERVICE) {
+				ct_state->backend_id = entry->rx_bytes;
+			}
 		}
 
 #ifdef ENABLE_NAT46
@@ -604,6 +614,37 @@ static inline void __inline__ ct_update6_slave(void *map,
 	return;
 }
 
+static inline void __inline__ ct_update6_backend_id(void *map,
+						    struct ipv6_ct_tuple *tuple,
+						    struct ct_state *state)
+{
+	struct ct_entry *entry;
+
+	entry = map_lookup_elem(map, tuple);
+	if (!entry)
+		return;
+
+	/* See the ct_create4 comments re the rx_bytes hack */
+	entry->rx_bytes = state->backend_id;
+	return;
+}
+
+static inline void __inline__
+ct_update6_slave_and_backend_id(void *map,
+				struct ipv6_ct_tuple *tuple,
+				struct ct_state *state)
+{
+	struct ct_entry *entry;
+
+	entry = map_lookup_elem(map, tuple);
+	if (!entry)
+		return;
+
+	entry->slave = state->slave;
+	/* See the ct_create4 comments re the rx_bytes hack */
+	entry->rx_bytes = state->backend_id;
+	return;
+}
 
 /* Offset must point to IPv6 */
 static inline int __inline__ ct_create6(void *map, struct ipv6_ct_tuple *tuple,
@@ -614,6 +655,11 @@ static inline int __inline__ ct_create6(void *map, struct ipv6_ct_tuple *tuple,
 	struct ct_entry entry = { };
 	bool is_tcp = tuple->nexthdr == IPPROTO_TCP;
 	union tcp_flags seen_flags = { .value = 0 };
+
+	/* See the ct_create4 comments re the rx_bytes hack */
+	if (dir == CT_SERVICE) {
+		entry.rx_bytes = ct_state->backend_id;
+	}
 
 	entry.rev_nat_index = ct_state->rev_nat_index;
 	entry.lb_loopback = ct_state->loopback;
@@ -673,6 +719,38 @@ static inline void __inline__ ct_update4_slave(void *map,
 	return;
 }
 
+static inline void __inline__ ct_update4_backend_id(void *map,
+						    struct ipv4_ct_tuple *tuple,
+						    struct ct_state *state)
+{
+	struct ct_entry *entry;
+
+	entry = map_lookup_elem(map, tuple);
+	if (!entry)
+		return;
+
+	/* See the ct_create4 comments re the rx_bytes hack */
+	entry->rx_bytes = state->backend_id;
+	return;
+}
+
+static inline void __inline__
+ct_update4_slave_and_backend_id(void *map,
+				struct ipv4_ct_tuple *tuple,
+				struct ct_state *state)
+{
+	struct ct_entry *entry;
+
+	entry = map_lookup_elem(map, tuple);
+	if (!entry)
+		return;
+
+	entry->slave = state->slave;
+	/* See the ct_create4 comments re the rx_bytes hack */
+	entry->rx_bytes = state->backend_id;
+	return;
+}
+
 static inline int __inline__ ct_create4(void *map, struct ipv4_ct_tuple *tuple,
 					struct __sk_buff *skb, int dir,
 					struct ct_state *ct_state)
@@ -682,8 +760,20 @@ static inline int __inline__ ct_create4(void *map, struct ipv4_ct_tuple *tuple,
 	bool is_tcp = tuple->nexthdr == IPPROTO_TCP;
 	union tcp_flags seen_flags = { .value = 0 };
 
-	entry.rev_nat_index = ct_state->rev_nat_index;
 	entry.lb_loopback = ct_state->loopback;
+
+	/* We need to store the backend_id (points to a svc v2 endpoint), while
+	 * keeping the slave field in tact for the backward compatibility.
+	 * Previously, the rx_bytes field was not used for entries with
+	 * the dir=CT_SERVICE (see GH#7060). Therefore, we can safely abuse
+	 * this field to save the backend_id. The hack will go away once we stop
+	 * supporting the legacy svc (in v1.6 we will populate rx_bytes and slave
+	 * with backend_id of svc v2, in v1.7 we will remove the rx_bytes hack).
+	 */
+	if (dir == CT_SERVICE) {
+		entry.rx_bytes = ct_state->backend_id;
+	}
+	entry.rev_nat_index = ct_state->rev_nat_index;
 	entry.slave = ct_state->slave;
 	seen_flags.value |= is_tcp ? TCP_FLAG_SYN : 0;
 	ct_update_timeout(&entry, is_tcp, dir, seen_flags);
@@ -774,6 +864,19 @@ static inline void __inline__ ct_update6_slave(void *map,
 {
 }
 
+static inline void __inline__ ct_update6_backend_id(void *map,
+						    struct ipv6_ct_tuple *tuple,
+						    struct ct_state *state)
+{
+}
+
+static inline void __inline__
+ct_update6_slave_and_backend_id(void *map,
+				struct ipv6_ct_tuple *tuple,
+				struct ct_state *state)
+{
+}
+
 static inline int __inline__ ct_create6(void *map, struct ipv6_ct_tuple *tuple,
 					struct __sk_buff *skb, int dir,
 					struct ct_state *ct_state)
@@ -784,6 +887,19 @@ static inline int __inline__ ct_create6(void *map, struct ipv6_ct_tuple *tuple,
 static inline void __inline__ ct_update4_slave(void *map,
 					       struct ipv4_ct_tuple *tuple,
 					       struct ct_state *state)
+{
+}
+
+static inline void __inline__ ct_update4_backend_id(void *map,
+						    struct ipv4_ct_tuple *tuple,
+					            struct ct_state *state)
+{
+}
+
+static inline void __inline__
+ct_update4_slave_and_backend_id(void *map,
+				struct ipv4_ct_tuple *tuple,
+				struct ct_state *state)
 {
 }
 

bpf/lib/dbg.h
@@ -46,12 +46,16 @@ enum {
 	DBG_LB6_LOOKUP_MASTER_FAIL,
 	DBG_LB6_LOOKUP_SLAVE,
 	DBG_LB6_LOOKUP_SLAVE_SUCCESS,
+	DBG_LB6_LOOKUP_SLAVE_V2_FAIL,
+	DBG_LB6_LOOKUP_BACKEND_FAIL,
 	DBG_LB6_REVERSE_NAT_LOOKUP,
 	DBG_LB6_REVERSE_NAT,
 	DBG_LB4_LOOKUP_MASTER,
 	DBG_LB4_LOOKUP_MASTER_FAIL,
 	DBG_LB4_LOOKUP_SLAVE,
 	DBG_LB4_LOOKUP_SLAVE_SUCCESS,
+	DBG_LB4_LOOKUP_SLAVE_V2_FAIL,
+	DBG_LB4_LOOKUP_BACKEND_FAIL,
 	DBG_LB4_REVERSE_NAT_LOOKUP,
 	DBG_LB4_REVERSE_NAT,
 	DBG_LB4_LOOPBACK_SNAT,

commit 6c1f0b00abaa0b1ef3328075addb280cde5151d5

cilium: sockmap fix compile warnings from lb services v2
Introduction of v2 services causes warnings when compiling sockops
code. Resolve here and update code to use v2 when needed.

Fixes: 01fd58a5d4203 ("bpf: Add support for svc v2 to datapath")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/sockops/bpf_sockops.c
@@ -48,20 +48,35 @@ static inline void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
 {
 	__u32 dip4, dport, dstID = 0;
 	struct endpoint_info *exists;
-	struct lb4_key lb4_key = {};
 	struct sock_key key = {};
-	struct lb4_service *svc;
 	int verdict;
 
 	sk_extract4_key(skops, &key);
-	sk_lb4_key(&lb4_key, &key);
 
 	/* If endpoint a service use L4/L3 stack for now. These can be
 	 * pulled in as needed.
 	 */
-	svc = __lb4_lookup_service(&lb4_key);
-	if (svc)
-		return;
+#ifdef ENABLE_LEGACY_SERVICES
+	{
+		struct lb4_key lb4_key = {};
+		struct lb4_service *svc
+
+		sk_lb4_key(&lb4_key, &key);
+		svc = __lb4_lookup_service(&lb4_key);
+		if (svc)
+			return;
+	}
+#else
+	{
+		struct lb4_key_v2 lb4_key = {};
+		struct lb4_service_v2 *svc;
+
+		sk_lb4_key_v2(&lb4_key, &key);
+		svc = __lb4_lookup_service_v2(&lb4_key);
+		if (svc)
+			return;
+	}
+#endif
 
 	/* Policy lookup required to learn proxy port */
 	if (1) {

bpf/sockops/bpf_sockops.h
@@ -75,14 +75,23 @@ static __always_inline void sk_msg_extract4_key(struct sk_msg_md *msg,
 	key->dport = msg->remote_port >> 16;
 }
 
+#ifdef ENABLE_LEGACY_SERVICES
 static __always_inline void sk_lb4_key(struct lb4_key *lb4,
 				       struct sock_key *key)
 {
 	/* SK MSG is always egress, so use daddr */
 	lb4->address = key->dip4;
 	lb4->dport = key->dport;
-	lb4->slave = 0;
 }
+#else
+static __always_inline void sk_lb4_key_v2(struct lb4_key_v2 *lb4,
+					  struct sock_key *key)
+{
+	/* SK MSG is always egress, so use daddr */
+	lb4->address = key->dip4;
+	lb4->dport = key->dport;
+}
+#endif
 
 static __always_inline bool redirect_to_proxy(int verdict)
 {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=80c3c50d9d3a80c89b1bd7d401f3a62b68633657
+GO_BINDATA_SHA1SUM=2ef9556bec3f7a33b5d2854e305a8590f09bd444
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 