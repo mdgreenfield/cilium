mdgreenfield/cilium

commit 6145cb56c89086bcbf9d76470abe328b4d8b3bd7

k8s: Merge initContainer cleanup with cilium cleanup
* Adds two `cilium cleanup` flags `--all-state`
  (env `CILIUM_ALL_STATE`) and `--bpf-state`
  (env `CILIUM_BPF_STATE`) to optionally remove
  all cilium state and only BPF related state respectively.
  The idea is to be able to honor both
  `clean-cilium-state` and `clean-cilium-bpf-state` options
  while replacing the init script logic.
* Replaces cleanup logic in `init-container.sh' with call to
  `cilium cleanup`.
* Changes the base docker image for `cilium-init` to use
  the cilium image in place of busybox in order to have access
  to the cilium binary from `init-container.sh`.
* Updates k8s YAMLs to use new cleanup environment variables.

Fixes #6478

Signed-off-by: ifeanyi <ify1992@yahoo.com>

cilium/cmd/cleanup.go
@@ -27,8 +27,10 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/netns"
+	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/vishvananda/netlink"
 )
 
@@ -42,7 +44,20 @@ var cleanupCmd = &cobra.Command{
 	},
 }
 
-var force bool
+var (
+	cleanAll bool
+	cleanBPF bool
+	force    bool
+)
+
+const (
+	allFlagName   = "all-state"
+	bpfFlagName   = "bpf-state"
+	forceFlagName = "force"
+
+	cleanCiliumEnvVar = "CLEAN_CILIUM_STATE"
+	cleanBpfEnvVar    = "CLEAN_CILIUM_BPF_STATE"
+)
 
 const (
 	ciliumLinkPrefix  = "cilium_"
@@ -56,17 +71,66 @@ const (
 
 func init() {
 	rootCmd.AddCommand(cleanupCmd)
-	cleanupCmd.Flags().BoolVarP(&force, "force", "f", false, "Skip confirmation")
+
+	cleanupCmd.Flags().BoolVarP(&cleanAll, allFlagName, "", false, "Remove all cilium state")
+	cleanupCmd.Flags().BoolVarP(&cleanBPF, bpfFlagName, "", false, "Remove BPF state")
+	cleanupCmd.Flags().BoolVarP(&force, forceFlagName, "f", false, "Skip confirmation")
+
+	option.BindEnv(allFlagName)
+	option.BindEnv(bpfFlagName)
+
+	bindEnv(cleanCiliumEnvVar, cleanCiliumEnvVar)
+	bindEnv(cleanBpfEnvVar, cleanBpfEnvVar)
+
+	if err := viper.BindPFlags(cleanupCmd.Flags()); err != nil {
+		Fatalf("viper failed to bind to flags: %v\n", err)
+	}
 }
 
-func runCleanup() {
-	// Abort if the agent is running, err == nil is handled correctly by Stat.
-	if _, err := os.Stat(defaults.PidFilePath); !os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "Agent should not be running when cleaning up\n"+
-			"Found pidfile %s\n", defaults.PidFilePath)
-		os.Exit(1)
+func bindEnv(flagName string, envName string) {
+	if err := viper.BindEnv(flagName, envName); err != nil {
+		Fatalf("Unable to bind flag %s to env variable %s: %s", flagName, envName, err)
 	}
+}
 
+// cleanupFunc represents a function to cleanup specific state items.
+type cleanupFunc func() error
+
+// cleanup represents a set of cleanup actions.
+type cleanup interface {
+	// whatWillBeRemoved contains descriptions of
+	// the removed state.
+	whatWillBeRemoved() []string
+	// cleanupFuncs contains a set of functions that
+	// carry out cleanup actions.
+	cleanupFuncs() []cleanupFunc
+}
+
+// bpfCleanup represents cleanup actions for BPF state.
+type bpfCleanup struct{}
+
+func (c bpfCleanup) whatWillBeRemoved() []string {
+	return []string{
+		fmt.Sprintf("all BPF maps in %s containing '%s' and '%s'",
+			bpf.MapPrefixPath(), ciliumLinkPrefix, tunnel.MapName),
+		fmt.Sprintf("mounted bpffs at %s", bpf.GetMapRoot()),
+	}
+}
+
+func (c bpfCleanup) cleanupFuncs() []cleanupFunc {
+	return []cleanupFunc{
+		removeAllMaps,
+	}
+}
+
+// ciliumCleanup represents cleanup actions for non-BPF state.
+type ciliumCleanup struct {
+	routes map[int]netlink.Route
+	links  map[int]netlink.Link
+	netNSs []string
+}
+
+func newCiliumCleanup() ciliumCleanup {
 	routes, links, err := findRoutesAndLinks()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
@@ -77,63 +141,125 @@ func runCleanup() {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 	}
 
-	showWhatWillBeRemoved(routes, links, netNSs)
+	return ciliumCleanup{routes, links, netNSs}
+}
+
+func (c ciliumCleanup) whatWillBeRemoved() []string {
+	toBeRemoved := []string{
+		fmt.Sprintf("mounted cgroupv2 at %s", defaults.DefaultCgroupRoot),
+		fmt.Sprintf("library code in %s", defaults.LibraryPath),
+		fmt.Sprintf("endpoint state in %s", defaults.RuntimePath),
+		fmt.Sprintf("CNI configuration at %s, %s, %s",
+			cniConfigV1, cniConfigV2, cniConfigV3),
+	}
+
+	if len(c.routes) > 0 {
+		section := "routes\n"
+		for _, v := range c.routes {
+			section += fmt.Sprintf("%v\n", v)
+		}
+		toBeRemoved = append(toBeRemoved, section)
+	}
+
+	if len(c.links) > 0 {
+		section := "links\n"
+		for _, v := range c.links {
+			section += fmt.Sprintf("%v\n", v)
+		}
+		toBeRemoved = append(toBeRemoved, section)
+	}
+
+	if len(c.netNSs) > 0 {
+		section := "network namespaces\n"
+		for _, n := range c.netNSs {
+			section += fmt.Sprintf("%s\n", n)
+		}
+		toBeRemoved = append(toBeRemoved, section)
+	}
+
+	return toBeRemoved
+}
+
+func (c ciliumCleanup) cleanupFuncs() []cleanupFunc {
+	cleanupRoutesAndLinks := func() error {
+		return removeRoutesAndLinks(c.routes, c.links)
+	}
+
+	cleanupNamedNetNSs := func() error {
+		return removeNamedNetNSs(c.netNSs)
+	}
+
+	return []cleanupFunc{
+		unmountCgroup,
+		removeDirs,
+		removeCNI,
+		cleanupRoutesAndLinks,
+		cleanupNamedNetNSs,
+	}
+}
+
+func runCleanup() {
+	// Abort if the agent is running, err == nil is handled correctly by Stat.
+	if _, err := os.Stat(defaults.PidFilePath); !os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Agent should not be running when cleaning up\n"+
+			"Found pidfile %s\n", defaults.PidFilePath)
+		os.Exit(1)
+	}
+
+	cleanAll = viper.GetBool(allFlagName) || viper.GetBool(cleanCiliumEnvVar)
+	cleanBPF = viper.GetBool(bpfFlagName) || viper.GetBool(cleanBpfEnvVar)
+
+	if cleanAll {
+		cleanBPF = true
+	}
+
+	var cleanups []cleanup
+
+	if cleanBPF {
+		cleanups = append(cleanups, bpfCleanup{})
+	}
+
+	if cleanAll {
+		cleanups = append(cleanups, newCiliumCleanup())
+	}
+
+	if len(cleanups) == 0 {
+		return
+	}
+
+	showWhatWillBeRemoved(cleanups)
 	if !force && !confirmCleanup() {
 		return
+	}
+
+	var cleanupFuncs []cleanupFunc
+	for _, cleanup := range cleanups {
+		cleanupFuncs = append(cleanupFuncs, cleanup.cleanupFuncs()...)
 	}
 
 	// ENOENT and similar errors are ignored. Should print all other
 	// errors seen, but continue.  So that one remove function does not
 	// prevent the remaining from running.
-	type cleanupFunc func() error
-	checks := []cleanupFunc{removeAllMaps, unmountCgroup, removeDirs, removeCNI}
-	for _, clean := range checks {
+	for _, clean := range cleanupFuncs {
 		if err := clean(); err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		}
 	}
-
-	if err := removeRoutesAndLinks(routes, links); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-	}
-
-	if err := removeNamedNetNSs(netNSs); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-	}
 }
 
-func showWhatWillBeRemoved(routes map[int]netlink.Route, links map[int]netlink.Link, netNSs []string) {
-	fmt.Printf("Warning: Destructive operation. You are about to remove:\n"+
-		"- all BPF maps in %s containing '%s' and '%s'\n"+
-		"- mounted bpffs at %s\n"+
-		"- mounted cgroupv2 at %s\n"+
-		"- library code in %s\n"+
-		"- endpoint state in %s\n"+
-		"- CNI configuration at %s, %s, %s\n",
-		bpf.MapPrefixPath(), ciliumLinkPrefix, tunnel.MapName, bpf.GetMapRoot(),
-		defaults.DefaultCgroupRoot, defaults.LibraryPath, defaults.RuntimePath,
-		cniConfigV1, cniConfigV2, cniConfigV3)
+func showWhatWillBeRemoved(cleanups []cleanup) {
+	var toBeRemoved []string
 
-	if len(routes) > 0 {
-		fmt.Printf("- routes\n")
-		for _, v := range routes {
-			fmt.Printf("%v\n", v)
-		}
+	for _, cleanup := range cleanups {
+		toBeRemoved = append(toBeRemoved, cleanup.whatWillBeRemoved()...)
 	}
 
-	if len(links) > 0 {
-		fmt.Println("- links")
-		for _, v := range links {
-			fmt.Printf("%v\n", v)
-		}
+	warning := "Warning: Destructive operation. You are about to remove:\n"
+	for _, warn := range toBeRemoved {
+		warning += fmt.Sprintf("- %s\n", warn)
 	}
 
-	if len(netNSs) > 0 {
-		fmt.Println("- network namespaces")
-		for _, n := range netNSs {
-			fmt.Printf("%s\n", n)
-		}
-	}
+	fmt.Printf(warning)
 }
 
 func confirmCleanup() bool {

contrib/packaging/docker/Dockerfile.init
@@ -2,9 +2,9 @@
 # Cilium init container.
 #
 # cilium-init is used in kubernetes environments to run miscellaneous
-# logic for cleaning up containers in between pod restarts.
+# logic for setting up containers in between pod restarts.
 #
-FROM docker.io/library/busybox:1.28.4
+FROM docker.io/cilium/cilium:latest
 LABEL maintainer="maintainer@cilium.io"
 COPY init-container.sh /init-container.sh
 CMD ["/init-container.sh"]

contrib/packaging/docker/init-container.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-if [ "${CLEAN_CILIUM_STATE}" = "true" ]; then
-    echo "Removing Cilium state..."
-    rm -rf /var/run/cilium/state;
-fi;
-if [ "${CLEAN_CILIUM_STATE}" = "true" ] \
-   || [ "${CLEAN_CILIUM_BPF_STATE}" = "true" ]; then
-    echo "Removing BPF state..."
-    rm -rf /sys/fs/bpf/tc/globals/cilium_* \
-           /var/run/cilium/bpffs/tc/globals/cilium_*;
-fi
+cilium cleanup -f
+
 if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
 	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
 fi;

examples/kubernetes/1.10/cilium-containerd-ds.yaml
@@ -147,13 +147,13 @@ spec:
       - command:
         - /init-container.sh
         env:
-        - name: CLEAN_CILIUM_STATE
+        - name: CILIUM_ALL_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-state
               name: cilium-config
               optional: true
-        - name: CLEAN_CILIUM_BPF_STATE
+        - name: CILIUM_BPF_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-bpf-state

examples/kubernetes/1.10/cilium-containerd.yaml
@@ -304,13 +304,13 @@ spec:
       - command:
         - /init-container.sh
         env:
-        - name: CLEAN_CILIUM_STATE
+        - name: CILIUM_ALL_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-state
               name: cilium-config
               optional: true
-        - name: CLEAN_CILIUM_BPF_STATE
+        - name: CILIUM_BPF_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-bpf-state

examples/kubernetes/1.10/cilium-crio-ds.yaml
@@ -155,13 +155,13 @@ spec:
       - command:
         - /init-container.sh
         env:
-        - name: CLEAN_CILIUM_STATE
+        - name: CILIUM_ALL_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-state
               name: cilium-config
               optional: true
-        - name: CLEAN_CILIUM_BPF_STATE
+        - name: CILIUM_BPF_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-bpf-state

examples/kubernetes/1.10/cilium-crio.yaml
@@ -312,13 +312,13 @@ spec:
       - command:
         - /init-container.sh
         env:
-        - name: CLEAN_CILIUM_STATE
+        - name: CILIUM_ALL_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-state
               name: cilium-config
               optional: true
-        - name: CLEAN_CILIUM_BPF_STATE
+        - name: CILIUM_BPF_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-bpf-state

examples/kubernetes/1.10/cilium-ds.yaml
@@ -154,13 +154,13 @@ spec:
       - command:
         - /init-container.sh
         env:
-        - name: CLEAN_CILIUM_STATE
+        - name: CILIUM_ALL_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-state
               name: cilium-config
               optional: true
-        - name: CLEAN_CILIUM_BPF_STATE
+        - name: CILIUM_BPF_STATE
           valueFrom:
             configMapKeyRef:
               key: clean-cilium-bpf-state

commit f5feac4b173705335b77d2b781eb98e14df636c3

cli: Restore cilium cleanup behaviour
when no flags(--all-state|--bpf-state) are specified fallback to
--all-state to keep the previous cilium cleanup behaviour

Fixes: 6145cb56c89086 ("k8s: Merge initContainer cleanup with cilium cleanup")

Signed-off-by: Nirmoy Das <ndas@suse.de>

cilium/cmd/cleanup.go
@@ -209,13 +209,14 @@ func runCleanup() {
 	cleanAll = viper.GetBool(allFlagName) || viper.GetBool(cleanCiliumEnvVar)
 	cleanBPF = viper.GetBool(bpfFlagName) || viper.GetBool(cleanBpfEnvVar)
 
-	if cleanAll {
-		cleanBPF = true
+	// if no flags are specifed then clean all
+	if (cleanAll || cleanBPF) == false {
+		cleanAll = true
 	}
 
 	var cleanups []cleanup
 
-	if cleanBPF {
+	if cleanAll || cleanBPF {
 		cleanups = append(cleanups, bpfCleanup{})
 	}
 